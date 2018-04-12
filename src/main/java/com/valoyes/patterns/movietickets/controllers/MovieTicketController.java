ef4ba12caf54a44d87f301395ba0659835fabe6a
bavaloyes <bavaloyes@ITEM-S35533.emea.msad.sopra>
2018-04-12 19:14:11 +0200

    add mapping for from submit

--- src/main/java/com/valoyes/patterns/movietickets/controllers/MovieTicketController.java
+++ src/main/java/com/valoyes/patterns/movietickets/controllers/MovieTicketController.java
@@ -1,15 +1,44 @@
 package com.valoyes.patterns.movietickets.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.valoyes.patterns.movietickets.dao.MovieTicketDAO;
+import com.valoyes.patterns.movietickets.entities.MovieTicket;
 
 // controller annotation from Spring
 @Controller
 public class MovieTicketController {
 
+	@Autowired
+	private MovieTicketDAO dao;
+	
 	@RequestMapping("/showCreateTicket")
 	public String showCreateTicket() {
 		
+		return "createTicket";
+	}
+	
+	/**
+	 * Spring de manera automatica, va a tomar la informacion que sale de nuestro form
+	 * y con ella creare una instancia de MovieTicket.
+	 * 
+	 * Este metodo persiste el ticket en la base de datos y reenvia al usuario a la pagina
+	 * createTicket.jsp
+	 * 
+	 * El nombre de esta clase coincide con <form action="createTicket"
+	 * 
+	 * @param ticket
+	 * @return
+	 */
+	@RequestMapping("/createTicket")
+	public String createTicket(MovieTicket ticket, ModelMap modelMap) {
+		
+		dao.create(ticket);
+		modelMap.addAttribute("msg", "Se ha realizado con exito la compra del ticket");
+		// seguumos volviendo a la pagina de entrada
 		return "createTicket";
 	}
 }

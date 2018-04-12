8f7c5a25f8b5253bbeefec0fc3a9a82a814fac35
bavaloyes <bavaloyes@ITEM-S35533.emea.msad.sopra>
2018-04-12 18:40:27 +0200

    init commit other files

---
+++ src/main/java/com/valoyes/patterns/movietickets/MovieTicketsApplication.java
@@ -0,0 +1,12 @@
+package com.valoyes.patterns.movietickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketsApplication.class, args);
+	}
+}
